374050dbf20b81879f7c66a353b9f18d4a4da4eb
AlanGit-2003 <alegre.alan2003@gmail.com>
2026-06-01 23:54:35 -0300

    5 commit

--- pc2.h
+++
@@ -1,9 +0,0 @@
-#ifndef PC2_H_INCLUDED
-#define PC2_H_INCLUDED
-
-#include <string>
-
-void productor(std::string id, int cantidadProductores);
-void consumidor(std::string id, int cantidadConsumidores);
-
-#endif // PC2_H_INCLUDED
